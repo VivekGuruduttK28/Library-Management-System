9270fc2a35b97aefe50e80f4d7274929869b5740
Vivek Gurudutt K <127002789+VivekGuruduttK28@users.noreply.github.com>
2025-03-10 08:20:24 +0530

    Delete library-management-system directory

--- library-management-system/src/main/java/com/project/library_management_system/dto/LoginDTO.java
+++
@@ -1,9 +0,0 @@
-package com.project.library_management_system.dto;
-
-import lombok.Data;
-
-@Data
-public class LoginDTO {
-    private String email;
-    private String password;
-}
--- library-management-system/src/main/java/com/project/library_management_system/services/AuthService.java
+++
@@ -1,7 +0,0 @@
-package com.project.library_management_system.services;
-
-import com.project.library_management_system.dto.LoginDTO;
-
-public interface AuthService {
-    String loginUser(LoginDTO loginDTO);
-}
